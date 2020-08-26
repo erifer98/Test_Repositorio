40f0fdafa36e1b513a8fe6552d8364c9b64b41b6
dayradlt <dayra.delatorre25@gmail.com>
2020-08-25 23:33:49 -0500

    Cambio 2

---
+++ TDD_test_Calculadora.py
@@ -0,0 +1,9 @@
+import unittest
+
+from Calculadora import Calculadora
+
+class Test_1 (unittest.TestCase):
+    def Calculadora_debe_iniciar_en_cero(self):
+        calc=Calculadora()
+        self.assertEqual(0,calc.valor())
+        
