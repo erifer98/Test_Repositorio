08c720bd04771193e512b33141e9eb021f4670ef
Dayra De la Torre <42183375+dayradlt@users.noreply.github.com>
2020-08-30 18:28:13 -0500

    Update TDD_test_Calculadora.py

--- TDD_test_Calculadora.py
+++ TDD_test_Calculadora.py
@@ -3,11 +3,14 @@ from Calculadora import calculadora
 
 class TestCal(unittest.TestCase):
 
-    def test_debe_iniciar_con_uno(self):
+       def test_debe_iniciar_con_uno(self):
         calc = calculadora()
         self.assertEqual(0,calc.valor())
     
-    
+    def test_suma_2_mas_3_es_cinco(self):
+        calc= calculadora()
+        calc.suma(2,3)
+        self.assertEqual(5,calc.valor())
 
 if __name__=='__main__':
     unittest.main()
